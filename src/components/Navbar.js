f615eee8e315ed2f172a8b750315ae4ff59edf8d
yidwang65 <yidwang@bu.edu>
2022-06-15 18:45:04 -0400

    finish implementing the navigation bar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
+import './Navbar.css';
+import {Button} from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-
+    const [button, setButton] = useState(true);
+  
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+  
+    const showButton = () => {
+      if (window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+  
+    useEffect(() => {
+      showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                     Icon
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars' }/>
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-items'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            home
+                        </Link>
+                    </li>
+                    <li className='nav-items'>
+                        <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                            projects
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     </>
